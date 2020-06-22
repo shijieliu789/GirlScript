bfbe726a2353d6f91716623b186b3d7f1d9f56dd
shijieliu789 <48249646+shijieliu789@users.noreply.github.com>
2020-06-22 11:01:51 +0100

    Add files via upload
    Hello World program and first week script

---
+++ HelloWorld.java
@@ -0,0 +1,71 @@
+
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        String c = "l";
+        int x = 10;
+        float y =13;
+        double z = 13.14;
+        char character = '*';
+
+        boolean j = false;
+
+
+        double sum = x + y - z;
+
+        if (sum > 22){
+            j = true;
+        }
+
+        if (j==true){
+            System.out.println("your sum is greater than 22!");
+        }else{
+            System.out.println("Your number is too small");
+        }
+
+//
+//        System.out.println("Hello World!" + " I am Jack");
+//
+//        System.out.println("hello again!");
+//
+//        boolean P = false;
+//        int x = 10;
+//
+//        if ((P == true) && (x > 10)){
+//            System.out.println("correct");
+//        }if (P == true && x < 10){
+//            System.out.println("You're in the else case!");
+//        }else{
+//            System.out.println("No cases satisfied.");
+//        }
+
+
+
+
+
+
+//        int x=0;
+//        int y;
+//        for (x=0; x<5; x++){
+//            System.out.print("|");
+//            if (x == 0 || x == 4) {
+//                for (y = 0; y < 10; y++) {
+//                    System.out.print("-");
+//                }
+//            }
+//            else {
+//                for (y=0;y<10;y++){
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println("|");
+//        }
+//
+//        while(i<25){
+//            System.out.println(i);
+//            i++;
+//        }
+    }
+}
